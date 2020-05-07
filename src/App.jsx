f0ff898f09df9398966f9b35701736d857f2168c
blazerius54 <79137837220@yandex.ru>
2020-05-08 01:23:47 +0300

    add content_security_policy and npm lib for yt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 import './index.css';
 
 export default class App extends React.Component {
-  render() {
+  state = {player: null};
+
+  setPlayer = (e) => {
+    this.setState({
+      player: e.target,
+    })
+  };
+
+  render () {
     return (
-      <div className="App">Hello extension</div>
+      <div className='App'>
+        <YouTube
+          videoId="2g811Eo7K8U"
+          onReady={this.setPlayer}
+        />
+        <button onClick={() => {
+          console.log(this.state);
+          this.state.player.playVideo();
+        }}>
+          play video
+        </button>
+        <button onClick={() => {
+          console.log(this.state);
+          this.state.player.pauseVideo();
+        }}>
+          pause video
+        </button>
+      </div>
     )
   }
 }
